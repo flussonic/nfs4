1bbb9c4a9c905394d4bc9912db9df94e9d9d2346
Max Lapshin <max@maxidoors.ru>
2020-08-18 10:47:12 +0300

    Now we can accept mount requests and send read bytes

--- src/nfs4_server.erl
+++ src/nfs4_server.erl
@@ -1,5 +1,6 @@
 -module(nfs4_server).
 -include_lib("kernel/include/logger.hrl").
+-include_lib("kernel/include/file.hrl").
 -include("nfs4.hrl").
 
 -export([start_link/4]).
@@ -11,6 +12,8 @@
 	socket,
 
 	%% logic
+	opened,
+	root,
 	clientid,
 	title,
 	callback,
@@ -18,6 +21,8 @@
 	verifier,
 	handles = []
 }).
+
+-define(ROOT, <<".">>).
 
 
 start_link(ListenerPid, Socket, Transport, []) ->
@@ -29,10 +34,12 @@ init_server(ListenerPid, Transport, Socket) ->
   ranch:accept_ack(ListenerPid),
   Transport:setopts(Socket, [{active,once},binary,{packet,sunrm}]),
   [_, Pid1,Pid2] = string:tokens(pid_to_list(self()),"<.>"),
-  Instanceid = list_to_integer(Pid2) bsl 32,
-  ClientId = Instanceid + list_to_integer(Pid1), 
+  Instanceid = list_to_integer(Pid1) *10000000,
+  ClientId = list_to_integer(Pid2) bsl 32 + list_to_integer(Pid1), 
+  {ok, Cwd} = file:get_cwd(),
   State = #state{
   	socket = Socket,
+  	root = Cwd,
   	instanceid = Instanceid,
   	clientid = ClientId
   },
@@ -99,8 +106,17 @@ handle_msg(Msg, S) ->
 
 
 -record(ctx, {
-	path = <<"/">>,
+	status = 'NFS4_OK',
+	path = <<"">>,
 	fh
+}).
+
+-record(file, {
+	path,
+	effective_path,
+	f,
+	stateid,
+	seq = 0
 }).
 
 
@@ -110,9 +126,9 @@ handle_funcall(0, _, S) ->
 handle_funcall(1, Bin, S) ->
   {CompoundArgs, _Offset} = nfs4_xdr:dec_COMPOUND4args(Bin, 0),
   {Tag, _MinorVersion, Commands} = CompoundArgs,
-	io:format("Call\n~p\n", [Commands]),
-  {Replies, _Ctx, S1} = handle_compound_call(Commands, #ctx{}, S),
-  Reply = nfs4_xdr:enc_COMPOUND4res({'NFS4_OK', Tag, Replies}),
+	% io:format("Call\n~p\n", [Commands]),
+  {Replies, #ctx{status = Status}, S1} = handle_compound_call(Commands, #ctx{}, S),
+  Reply = nfs4_xdr:enc_COMPOUND4res({Status, Tag, Replies}),
 	{reply, Reply, S1}.
 
 
@@ -120,46 +136,280 @@ handle_compound_call([], Ctx, S) ->
 	{[], Ctx, S};
 
 handle_compound_call([{Command, Args} | List], Ctx, S) ->
-	{reply, Reply, Ctx1, S1} = handle_one_call(Command, Args, Ctx, S),
-	{Replies, Ctx2, S2} = handle_compound_call(List, Ctx1, S1),
-	{[{Command,Reply}|Replies], Ctx2, S2}.
+	case handle_one_call(Command, Args, Ctx, S) of
+		{reply, Reply, Ctx1, S1} ->
+			% ?LOG_ERROR("~p(~p) = ~p", [Command, Args, Reply]),
+			{Replies, Ctx2, S2} = handle_compound_call(List, Ctx1, S1),
+			{[{Command,Reply}|Replies], Ctx2, S2};
+		{error, Error, Ctx1, S1} ->
+			% ?LOG_ERROR("~p(~p) ! ~p", [Command, Args, Error]),
+			{[{Command, Error}], Ctx1, S1}
+	end.
+
 
 
 
 handle_one_call('OP_SETCLIENTID', Args, Ctx, #state{clientid = ClientID} = S) ->
 	{{Verifier, ClientTitle}, Callback, _Ident} = Args,
-	io:format("clienid: ~p\n", [ClientTitle]),
 	{reply, {'NFS4_OK', {ClientID, Verifier}}, Ctx, S#state{verifier = Verifier, title = ClientTitle, callback = Callback}};
 
 handle_one_call('OP_SETCLIENTID_CONFIRM', _Args, Ctx, #state{} = S) ->
 	{reply, {'NFS4_OK'}, Ctx, S};
 
-handle_one_call('OP_PUTROOTFH', _, Ctx, #state{handles = H, instanceid = I} = S) ->
-	Handles1 = lists:keystore(<<"/">>, 1, H, {<<"/">>, I}),
-	{reply, {'NFS4_OK'}, Ctx#ctx{path = <<"/">>, fh = I}, S#state{handles = Handles1}};
+handle_one_call('OP_PUTROOTFH', _, Ctx, #state{} = S) ->
+	{FH, S1} = find_or_alloc_fh(?ROOT, S),
+	{reply, {'NFS4_OK'}, Ctx#ctx{path = ?ROOT, fh = FH}, S1};
 
-handle_one_call('OP_GETATTR', {RequestedAttrs}, #ctx{path = Path} = Ctx, #state{} = S) ->
-	io:format("requested attrs: ~p\n", [RequestedAttrs]),
-	Attrs = {[], [<<>>]},
-	{reply, {'NFS4_OK', {Attrs}}, Ctx, S};
+handle_one_call('OP_GETATTR', {RequestedAttrs}, #ctx{path = Path} = Ctx, #state{root = Root} = S) ->
+	EffectivePath = filename:join(Root, Path),
+	case file:read_file_info(EffectivePath) of
+		{ok, #file_info{} = FileInfo} ->
+			Attrs = getattr_on_file_info(RequestedAttrs, FileInfo, Ctx),
+			{reply, {'NFS4_OK', {Attrs}}, Ctx, S};
+		{error, _} ->
+			?LOG_ERROR("Strange, but cannot getattr on ~p (~s)", [Path, EffectivePath]),
+			{error, {'NFS4ERR_NOENT', void}, Ctx#ctx{status = 'NFS4ERR_NOENT'}, S}
+	end;
 
-handle_one_call('OP_GETFH', _, #ctx{path = Path} = Ctx, #state{handles = Handles, instanceid = I} = S) ->
-	{FH, Handles1} = case lists:keyfind(Path, 1, Handles) of
+handle_one_call('OP_GETFH', _, #ctx{path = Path} = Ctx, #state{} = S) ->
+	{FH, S1} = find_or_alloc_fh(Path, S),
+	{reply, {'NFS4_OK', {FH}}, Ctx#ctx{fh = FH}, S1};
+
+handle_one_call('OP_PUTFH', {FH}, #ctx{} = Ctx, #state{handles = Handles} = S) ->
+	case lists:keyfind(FH, 2, Handles) of
 		false ->
-			FH1 = length(Handles)+1+I,
-			{FH1, [{Path,FH1}|Handles]};
-		{Path,FH1} ->
-			{FH1, Handles}
-	end,
-	FH2 = integer_to_binary(FH),
-	{reply, {'NFS4_OK', {FH2}}, Ctx#ctx{fh = FH2}, S#state{handles = Handles1}};
+			?LOG_ERROR("stale fh ~p", [FH]),
+			{error, {'NFS4ERR_STALE'}, Ctx#ctx{status = 'NFS4ERR_STALE'}, S};
+		{Path, _} ->
+			% ?LOG_NOTICE("cd ~p ~p", [Path, FH]),
+			{reply, {'NFS4_OK'}, Ctx#ctx{fh = FH, path = Path}, S}
+	end;
 
-handle_one_call('OP_PUTFH', {FH}, #ctx{} = Ctx, #state{} = S) ->
-	{reply, {'NFS4_OK'}, Ctx#ctx{fh = FH}, S};
+handle_one_call('OP_LOOKUP', {Segment}, #ctx{path = Path} = Ctx, #state{root = Root} = S) ->
+	false = Segment == <<"..">>,
+	nomatch = binary:match(Segment,<<"/">>),
+	NewPath = filename:join(Path, Segment),
+	EffectivePath = filename:join([Root, Path, Segment]),
+	case file:read_file_info(EffectivePath) of
+		{ok, _} ->
+			{FH, S1} = find_or_alloc_fh(NewPath, S),
+			{reply, {'NFS4_OK'}, Ctx#ctx{path = NewPath, fh = FH}, S1};
+		{error, E} ->
+			?LOG_ERROR("~p ~s / ~s / ~s", [E, Root, Path, Segment]),
+			{error, {'NFS4ERR_NOENT'}, Ctx#ctx{status = 'NFS4ERR_NOENT'}, S}
+	end;
+
+handle_one_call('OP_OPEN', Args, #ctx{path = Path} = Ctx, #state{} = S) ->
+	{_Seq, _ShareAccess, _ShareDeny, _Owner, _Openhow, OpenClaim} = Args,
+	io:format("open_args: ~p\n", [Args]),
+	{'CLAIM_NULL', Segment} = OpenClaim,
+	SubPath = filename:join(Path, Segment),
+	case find_or_open(SubPath, S) of
+		{ok, #file{} = File, S1} ->
+			OpenOk = {
+				{File#file.seq, File#file.stateid},
+				{false,0,0}, % change
+				0, % result flags
+				[], % attributes for create
+				{'OPEN_DELEGATE_NONE', void}
+			},
+			{FH, S2} = find_or_alloc_fh(SubPath, S1),			
+			{reply, {'NFS4_OK',OpenOk}, Ctx#ctx{fh = FH, path = SubPath}, S2};
+		{error, _} ->
+			{error, {'NFS4ERR_NOENT',void}, Ctx#ctx{status = 'NFS4ERR_NOENT'}, S}
+	end;
+
+
+handle_one_call('OP_CLOSE', {_Seq, {_Seq2, StateId} = St}, #ctx{} = Ctx, #state{} = S) ->
+	case close_file(StateId, S) of
+		{ok, NewSubPath, S1} ->
+			{FH, S2} = find_or_alloc_fh(NewSubPath, S1),			
+			{reply, {'NFS4_OK',St}, Ctx#ctx{fh = FH, path = NewSubPath}, S2};
+		{error, _} ->
+			{error, {'NFS4ERR_NOENT',void}, Ctx#ctx{status = 'NFS4ERR_NOENT'}, S}
+	end;
+
+
+handle_one_call('OP_READDIR', Args, #ctx{path = Path} = Ctx, #state{root = Root} = S) ->
+	EffectivePath = iolist_to_binary(filename:join([Root, Path])),
+	List = filelib:wildcard("*", binary_to_list(EffectivePath)),
+	{Cookie, _, _Limit1, _Limit2, RequestedAttrs} = Args,
+	%Cookie = 769668192873268725,
+	Verifier = <<0:64>>,
+	{DirList, S3} = lists:foldr(fun(Name, {Tail, S1}) ->
+		case file:read_file_info(filename:join(EffectivePath,Name)) of
+			{ok, FileInfo} ->
+				VPath = filename:join(Path,Name),
+				{FH, S2} = find_or_alloc_fh(VPath, S1),
+				Attrs = getattr_on_file_info(RequestedAttrs, FileInfo, Ctx#ctx{path = VPath, fh = FH}),
+				{{Cookie, Name, Attrs, Tail}, S2};
+			{error, _} ->
+				{Tail, S1}
+		end
+	end, {void, S}, List),
+	% io:format("dirlist: ~p, ~p", [S3#state.handles, List]),
+	{reply, {'NFS4_OK', {Verifier, {DirList,true}}}, Ctx, S3};
 
 handle_one_call('OP_RENEW', _, #ctx{} = Ctx, #state{} = S) ->
 	{reply, {'NFS4_OK'}, Ctx, S};
 
+handle_one_call('OP_ACCESS', {Access}, #ctx{} = Ctx, #state{} = S) ->
+	{reply, {'NFS4_OK', {Access, 1 bor 2}}, Ctx, S};
+
+handle_one_call('OP_READ', {{_,StateId}, Offset, Count}, #ctx{} = Ctx, #state{} = S) ->
+	case pread(StateId, Offset, Count, S) of
+		{ok, Bin} ->
+			{reply, {'NFS4_OK', {false, Bin}}, Ctx, S};
+		eof ->
+			{reply, {'NFS4_OK', {true, <<>>}}, Ctx, S};
+		{error, _E} ->
+			{error, {'NFS4ERR_IO', vpid}, Ctx#ctx{status = 'NFS4ERR_IO'}, S}
+	end;
+
 handle_one_call(Cmd, Args, Ctx, #state{} = S) ->
 	io:format("unknown ~p(~p)\n", [Cmd, Args]),
 	{reply, {'NFS4_OK'}, Ctx, S}.
+
+
+
+
+find_or_open(SubPath, #state{root = Root} = S) ->
+	EffectivePath = filename:join([Root, SubPath]),
+	case file:open(EffectivePath, [read,binary]) of
+		{ok, F} ->
+			File = #file{
+				effective_path = EffectivePath,
+				path = SubPath,
+				f = F,
+				seq = 1,
+				stateid = crypto:strong_rand_bytes(12)
+			},
+			{ok, File, S#state{opened = File}};
+		{error, E} ->
+			{error, E}
+	end.
+
+close_file(StateId, #state{opened = Opened} = S) ->
+	case Opened of
+		#file{f = F, stateid = StateId, path = SubPath} ->
+			NewSubPath = filename:dirname(SubPath),
+			file:close(F),
+			{ok, NewSubPath, S#state{opened = undefined}};
+		_ ->
+			{error, enoent}
+	end.
+
+pread(StateId, Offset, Count, #state{opened = Opened}) ->
+	case Opened of
+		#file{f = F, stateid = StateId} ->
+			file:pread(F, Offset, Count);
+		_ ->
+			{error, enoent}
+	end.
+
+
+
+
+
+find_or_alloc_fh(Path, #state{handles = Handles, instanceid = I} = S) ->
+	{FH, Handles1} = case lists:keyfind(Path, 1, Handles) of
+		false ->
+			FH1 = integer_to_binary(I + length(Handles)+1),
+			{FH1, [{Path,FH1}|Handles]};
+		{Path,FH1} ->
+			{FH1, Handles}
+	end,
+	{FH, S#state{handles = Handles1}}.
+
+
+
+
+
+getattr_on_file_info(RequestedAttrs, FileInfo, #ctx{path = Path, fh = FH}) ->
+	#file_info{inode = Inode, size = Size, type = Type, mode = Mode} = FileInfo,
+	Attrs = lists:flatmap(fun
+		(supported_attrs) ->
+			[{supported_attrs, [supported_attrs,type,fh_expire_type,change,size,link_support,symlink_support,
+			named_attr,fsid,unique_handles,lease_time,rdattr_error,acl,aclsupport,
+			cansettime,case_insensitive,case_preserving,chown_restricted,filehandle,fileid,
+			files_avail,files_total,fs_locations,homogeneous,maxfilesize, maxlink,maxname,
+			maxread,maxwrite,mode,no_trunc,numlinks,owner,owner_group,
+			rawdev,space_avail,space_free,space_total,space_used,time_access,time_access_set,
+			time_delta,time_metadata,time_modify,time_modify_set,mounted_on_fileid]}];
+		(type) ->
+			T = case Type of
+				device -> 'NF4BLK';
+				directory -> 'NF4DIR';
+				_ -> 'NF4REG'
+			end,
+			[{type, T}];
+		(fh_expire_type) ->
+			[{fh_expire_type, [volatile_any, noexpire_with_open]}];
+		(change) ->
+			[{change, 0}];
+		(size) ->
+			[{size, Size}];
+		(link_support) ->
+			[{link_support, true}];
+		(symlink_support) ->
+			[{symlink_support, true}];
+		(named_attr) ->
+			[{named_attr, false}];
+		(fsid) ->
+			[{fsid, {0,0}}];
+		(unique_handles) ->
+			[{unique_handles, true}];
+		(lease_time) ->
+			[{lease_time, 90}];
+		% (rdattr_error) ->
+		% 	[{rdattr_error, 'NFS4_OK'}];
+		(filehandle) ->
+			[{filehandle, FH}];
+		% (aclsupport) ->
+		% 	[{aclsupport, 0}];
+		(cansettime) ->
+			[{cansettime, false}];
+		(fileid) ->
+			[{fileid, Inode}];
+		(mode) ->
+			[{mode, Mode band 8#777}];
+		(numlinks) ->
+			[{numlinks, 1}];
+		(owner) ->
+			[{owner,<<"0">>}];
+		(owner_group) ->
+			[{owner_group,<<"0">>}];
+		(files_total) ->
+			[{files_total, 1024}];
+		(files_avail) ->
+			[{files_avail, 1024}];
+		(homogeneous) ->
+			[{homogeneous, true}];
+		(no_trunc) ->
+			[{no_trunc, false}];
+		(rawdev) ->
+			[{rawdev, {0,0}}];
+		(time_access) ->
+			[{time_access, {1500000000,0}}];
+		(time_modify) ->
+			[{time_modify, {1500000000,0}}];
+		(time_metadata) ->
+			[{time_metadata, {1500000000,0}}];
+		(space_used) ->
+			[{space_used, Size}];
+		(mounted_on_fileid) when Path == <<"">> ->
+			[{mounted_on_fileid,Inode}];
+		(maxfilesize) ->
+			[{maxfilesize,1024*1024*1024*128}];
+		(maxread) ->
+			[{maxread,131072}];
+		(maxwrite) ->
+			[{maxwrite,131072}];
+		(time_delta) ->
+			[{time_delta, {0,0}}];
+		(_) ->
+			[]
+	end, RequestedAttrs),
+	% io:format("unhandled attrs: ~p\n", [RequestedAttrs -- [T || {T,_} <- Attrs] ]),
+	% io:format("~s: ~p, ~p\n", [Path, Attrs, RequestedAttrs -- [T || {T,_} <- Attrs]]),
+	Attrs.
